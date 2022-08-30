3c3f750b55f8c73ab24e03c44c060b175e47efb3
Andrey Pechennikov <ap@devandy.me>
2022-08-30 22:57:49 +0300

    Установил фреймворк yii2

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-
-use AndreyPechennikov\TaskForce\converter\CsvSqlConverter;
-
-$converter = new CsvSqlConverter('data/csv');
-$result = $converter->convertFiles('data/sql');
-
-var_dump($result);
