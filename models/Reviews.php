d1044fcba5e2b947074c93d695b91dffcefac99d
Andrey Pechennikov <ap@devandy.me>
2022-08-31 00:28:46 +0300

    Добавил модели базы данных

---
+++ models/Reviews.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "reviews".
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property string $description
+ * @property int $rating
+ * @property int $reviewer_id
+ *
+ * @property Users $reviewer
+ * @property Tasks $task
+ */
+class Reviews extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'reviews';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['task_id', 'description', 'rating', 'reviewer_id'], 'required'],
+            [['task_id', 'rating', 'reviewer_id'], 'integer'],
+            [['description'], 'string', 'max' => 320],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Tasks::className(), 'targetAttribute' => ['task_id' => 'id']],
+            [['reviewer_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['reviewer_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'task_id' => 'Task ID',
+            'description' => 'Description',
+            'rating' => 'Rating',
+            'reviewer_id' => 'Reviewer ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Reviewer]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReviewer()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'reviewer_id']);
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Tasks::className(), ['id' => 'task_id']);
+    }
+}
---
+++ models/UserSpecializations.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_specializations".
+ *
+ * @property int $user_id
+ * @property int $specialization_id
+ *
+ * @property Specializations $specialization
+ * @property Users $user
+ */
+class UserSpecializations extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_specializations';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'specialization_id'], 'required'],
+            [['user_id', 'specialization_id'], 'integer'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['specialization_id'], 'exist', 'skipOnError' => true, 'targetClass' => Specializations::className(), 'targetAttribute' => ['specialization_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'specialization_id' => 'Specialization ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Specialization]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSpecialization()
+    {
+        return $this->hasOne(Specializations::className(), ['id' => 'specialization_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
+    }
+}
